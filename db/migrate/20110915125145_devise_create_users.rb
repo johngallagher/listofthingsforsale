372c7eecdb7c7268084d04c95c0a379984c8d664
John Gallagher <john@jgediting.co.uk>
2011-09-15 22:33:12 +0100

    Made users confirmable now.

--- db/migrate/20110915125145_devise_create_users.rb
+++ db/migrate/20110915125145_devise_create_users.rb
@@ -2,12 +2,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
+      t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
 
       # t.encryptable
-      # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110915125956) do
+ActiveRecord::Schema.define(:version => 20110915130532) do
 
   create_table "descriptions", :force => true do |t|
     t.text     "items_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", :force => true do |t|
+    t.string   "name"
+    t.text     "description_text"
+    t.decimal  "price"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", :force => true do |t|
+    t.string   "thumb_url"
+    t.string   "small_url"
+    t.string   "large_url"
+    t.string   "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -29,6 +47,9 @@ ActiveRecord::Schema.define(:version => 20110915125956) do
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
