1ead28c5f71094c32aeb6622714c131b81e3caa9
John Gallagher <john@jgediting.co.uk>
2011-09-27 00:09:07 +0100

    Testing paypal stuff - added test gateway to environment.

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -10,18 +10,18 @@ class PaymentController < ApplicationController
     logger.debug("Before acknowledge: #{notify.inspect}")
     
     if notify.acknowledge
-      logger.debug("After acknowledge!")
+      logger.debug("Acknowledged!")
       # @payment = Payment.find_by_confirmation(notify.transaction_id) ||
       #   enrollment.invoice.payments.create(:amount => notify.amount,
       #     :payment_method => 'paypal', :confirmation => notify.transaction_id,
       #     :description => notify.params['item_name'], :status => notify.status,
       #     :test => notify.test?)
-      # begin
-      #   if notify.complete?
-      #     @payment.status = notify.status
-      #   else
-      #     logger.error("Failed to verify Paypal's notification, please investigate")
-      #   end
+      begin
+        if notify.complete?
+          logger.debug("Hurrah - Status is #{notify.status}")
+        else
+          logger.error("Failed to verify Paypal's notification, please investigate")
+        end
       # rescue => e
       #   @payment.status = 'Error'
       #   raise
--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,7 @@ Things3::Application.initialize!
 Things3::Application.configure do
   config.action_mailer.delivery_method = :smtp
 end
+
+# Ensure the gateway is in test mode
+ ActiveMerchant::Billing::Base.gateway_mode = :test
+ ActiveMerchant::Billing::Base.integration_mode = :test
