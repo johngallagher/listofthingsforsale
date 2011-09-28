0304ff954cffab15c9aa7c42e57f783d741385a6
John Gallagher <john@jgediting.co.uk>
2011-09-29 00:56:07 +0100

    Merged the payment notification controller and my payment controller.

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,7 +1,32 @@
 class PaymentNotificationsController < ApplicationController  
   protect_from_forgery :except => [:create]  
-  def create  
-    PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )  
+  def create
+    logger.debug("Notified!")
+    notify = Paypal::Notification.new(request.raw_post)
+    # enrollment = Enrollment.find(notify.item_id)
+    logger.debug("Before acknowledge: #{notify.inspect}")
+    
+    if notify.acknowledge
+      logger.debug("Acknowledged!")
+      # @payment = Payment.find_by_confirmation(notify.transaction_id) ||
+      #   enrollment.invoice.payments.create(:amount => notify.amount,
+      #     :payment_method => 'paypal', :confirmation => notify.transaction_id,
+      #     :description => notify.params['item_name'], :status => notify.status,
+      #     :test => notify.test?)
+      PaymentNotification.create!(:params => notify.params, :status => notify.params[:payment_status], :transaction_id => notify.params[:txn_id] )
+      begin
+        if notify.complete?
+          logger.debug("Hurrah - success. Notify is #{notify.inspect}")
+        else
+          logger.error("Failed. Notify was: #{notify.inspect}")
+        end
+      rescue => e
+        raise
+      ensure
+      end
+    end
+    render :nothing => true
+    
     render :nothing => true  
   end  
 end
--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,12 +1,11 @@
 class PaymentNotification < ActiveRecord::Base
-  belongs_to :cart  
   serialize :params  
   after_create :mark_cart_as_purchased  
 
 private  
   def mark_cart_as_purchased  
     if status == "Completed"  
-      cart.update_attributes(:purchased_at => Time.now)  
+      # Send simple cart a js message to clear out the cart. Or maybe clear that cookie.
     end  
   end  
 end
