51af1182c30525adc348c1372e3d1be821b625a6
John Gallagher <john@jgediting.co.uk>
2011-10-01 14:35:30 +0100

    Adapter now sets up session id, shop id, status successfully.

--- test/unit/paypal_params_to_order_adapter_test.rb
+++ test/unit/paypal_params_to_order_adapter_test.rb
@@ -24,6 +24,7 @@ class PaypalParamsToOrderAdapterTest < ActiveSupport::TestCase
   setup do
     @matthias_shop = Factory(:matthias_shop)
     @order = Factory(:johns_pending_order_for_bag_and_wallet, :shop => @matthias_shop)
+    assert @order.shop_id != 0
   end
   
   test "should create order with same shop id" do
@@ -33,23 +34,29 @@ class PaypalParamsToOrderAdapterTest < ActiveSupport::TestCase
     
     adapted_shop = PaypalParamsToOrderAdapter.new(paypal_params).adapt
     
-    assert @order.shop_id != 0
     assert adapted_shop.shop_id == @order.shop_id
   end
   
-  # test "should create order with same status" do
-  #   paypal_params = PaypalParamsGenerator.new(:order => @order).generate_params
-  #   
-  #   assert PaypalParamsToOrderAdapter.new(paypal_params).adapt.status == Status::Pending
-  # end
-  # 
-  # test "should create order with same session id" do
-  #   paypal_params = PaypalParamsGenerator.new(:order => @order).generate_params
-  # 
-  #   assert order.session_id != ""
-  #   assert_not_nil order.session_id
-  #   
-  #   assert PaypalParamsToOrderAdapter.new(paypal_params).adapt.status == order.session_id
-  # end
+  test "should create order with same status if pending" do
+    paypal_params = PaypalParamsGenerator.new(:order => @order, :payment_status => Status::Pending).generate_params
+    adapted_shop = PaypalParamsToOrderAdapter.new(paypal_params).adapt
+    assert adapted_shop.status == Status::Pending
+  end
+  
+  test "should create order with same status if completed" do
+    paypal_params = PaypalParamsGenerator.new(:order => @order, :payment_status => Status::Completed).generate_params
+    adapted_shop = PaypalParamsToOrderAdapter.new(paypal_params).adapt
+    assert adapted_shop.status == Status::Completed
+  end
 
+  test "should create order with same session id" do
+    paypal_params = PaypalParamsGenerator.new(:order => @order).generate_params
+    adapted_shop = PaypalParamsToOrderAdapter.new(paypal_params).adapt
+
+    assert !@order.session_id.empty?
+    assert !@order.session_id.blank?
+    assert !@order.session_id.nil?
+    assert adapted_shop.session_id == @order.session_id
+  end
+  
 end
