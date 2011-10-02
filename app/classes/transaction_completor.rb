2ae4afa9563c3f1d284366a90a9b405a71d8b0ac
John Gallagher <john@jgediting.co.uk>
2011-10-02 17:18:00 +0100

    When something is sold out, it hides the add to cart function.

--- app/classes/transaction_completor.rb
+++ app/classes/transaction_completor.rb
@@ -7,7 +7,6 @@ class TransactionCompletor
     Rails.logger.debug("Before complete transaction")
     completed_order = OrderCompletor.new(:notification => @notification, :pending_order => @pending_order).complete_order
     StockManager.new(:completed_order => completed_order).adjust_stock
-    SimpleCartManager.clear_cart
     Rails.logger.debug("After complete transaction")
   end
 end
