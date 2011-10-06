09ebc0e804d228483c0e1de0c161724a95537be5
John Gallagher <john@jgediting.co.uk>
2011-10-06 20:45:13 +0100

    Fixed the session getting destroyed - added back in the csrf tags and all is well again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  private
+
+    # Overwriting the sign_out redirect path method
+    # def after_sign_out_path_for(resource_or_scope)
+    #   logger.debug "Heres the resource #{resource_or_scope.inspect}"
+    # end
 end
