c5b1ffd87378612578c951045f78fbb6da409dd8
John Gallagher <john@jgediting.co.uk>
2011-10-10 11:47:16 +0100

    Fixed facebooks tuff being wrong way round.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   Rails.logger.debug "Rails env is #{Rails.env.inspect}"
   if Rails.env.production?
-    provider :facebook, '258055290904307', '70219603c45d78038b3a8653cbd56591'
-  else
     provider :facebook, '175670452514877', '2a017518bf32595d84846f0e0bb8cfec'
+  else
+    provider :facebook, '258055290904307', '70219603c45d78038b3a8653cbd56591'
   end
 end
