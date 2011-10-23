0310c1215ae1c8d527561cf828799cdc0d1428d8
John Gallagher <john@jgediting.co.uk>
2011-10-24 00:38:12 +0100

    Made nice new profile pic that lines up perfectly with the box below. Looks killer.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,5 @@
 class Shop < ActiveRecord::Base
-  has_attached_file :profile_photo, :styles => { :profile => "230x55>" }, :storage => :s3, :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml", :path => "/:style/:id/:filename"
+  has_attached_file :profile_photo, :styles => { :profile => "270x220>" }, :storage => :s3, :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml", :path => "/:style/:id/:filename"
   has_friendly_id :url, :use_slug => true
 
   attr_accessor :description_for_shop, :publishing_errors
@@ -15,5 +15,9 @@ class Shop < ActiveRecord::Base
     self.payment_type ||= Payments::Paypal
     self.publishing_errors ||= []
     self.about_me ||= "Write whatever you want here. You could use it to tell people who you are and what you are selling. If you have a retail presence or a market stall, you could put up the details up here."
+  end
+  
+  def profile_photo_height
+    Paperclip::Geometry.from_file(profile_photo.to_file(:profile)).height
   end
 end
