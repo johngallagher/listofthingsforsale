35f4c511dd2ffb462bbcafaa9e9af8298b66ffa2
John Gallagher <john@jgediting.co.uk>
2011-10-24 23:49:38 +0100

    Removed debug logging from reg controller.

--- app/classes/registrations_controller.rb
+++ app/classes/registrations_controller.rb
@@ -4,18 +4,15 @@ class RegistrationsController < Devise::RegistrationsController
 
     if resource.save
       if resource.active_for_authentication?
-        logger.debug "---------1"
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
-        logger.debug "---------2"
         set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
         expire_session_data_after_sign_in!
         return render :json => {:success => true, :redirect => after_inactive_sign_up_path_for(resource)}
       end
     else
-      logger.debug "---------3"
       clean_up_passwords(resource)
       respond_with_navigational(resource) { render_with_scope :new }
     end
