ff55daf78d89eb1ad6e65821e474c1e265985a01
John Gallagher <john@jgediting.co.uk>
2011-10-30 01:48:33 +0100

    Added extra test with description.

--- test/unit/item_parser_test.rb
+++ test/unit/item_parser_test.rb
@@ -6,4 +6,10 @@ class ItemParserTest < ActiveSupport::TestCase
     assert parsed_item[:name] == "item name"
     assert parsed_item[:price] == "3.45"
   end
+  test "description should be parsed out" do
+    parsed_item = ItemParser.parse("item name $3.45 excellent condition")
+    assert parsed_item[:name] == "item name"
+    assert parsed_item[:price] == "3.45"
+    assert parsed_item[:description] == "excellent condition"
+  end
 end
