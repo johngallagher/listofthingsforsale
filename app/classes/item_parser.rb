581572afb5a683097ba68671355c84a6e62bfb6c
John Gallagher <john@jgediting.co.uk>
2011-10-30 02:30:14 +0000

    Added quantities and category and it works so far.

--- app/classes/item_parser.rb
+++ app/classes/item_parser.rb
@@ -2,6 +2,6 @@ class ItemParser
   def self.parse(description)
     Rails.logger.debug "About to match #{description}"
     # description.chomp.match(/(?<name>[[:print:]]+) \$(?<price>\d+\.*\d*)(?<description_text> ([^\+#][[:print:]])*)?(?<quantity>\+(\d+))?( #(?<cat1>[[:print:]][^#]*))?$/)
-    description.chomp.match(/(?<name>[[:print:]]+) \$(?<price>\d+\.*\d*)( +?(?<description_text>[^\+#][[:print:]]*))?( +?(\+(?<quantity>\d+)))?( +?#(?<cat1>[[:print:]][^#]*))?$/)
+    description.chomp.match(/(?<name>[[:print:]]+) \$(?<price>\d+\.*\d*)( +?(?<description_text>[^\+#]([[:print:]][^\+#])*))?( +?(\+(?<quantity>\d+)))?( +?#(?<cat1>[[:print:]][^#]*))?$/)
   end
 end
--- test/unit/item_parser_test.rb
+++ test/unit/item_parser_test.rb
@@ -25,30 +25,30 @@ class ItemParserTest < ActiveSupport::TestCase
     assert_equal "5",                     parsed_item[:quantity]
     assert_equal nil,                     parsed_item[:cat1]
   end
-  # test "name price description and quantity" do
-  #   parsed_item = ItemParser.parse("item name $3.45 excellent condition +5")
-  #   assert_equal "item name",             parsed_item[:name]
-  #   assert_equal "3.45",                  parsed_item[:price]
-  #   assert_equal " excellent condition",  parsed_item[:description_text]
-  #   assert_equal "5",                     parsed_item[:quantity]
-  #   assert_equal nil,                     parsed_item[:cat1]
-  # end
-  # test "name price quantity and category" do
-  #   parsed_item = ItemParser.parse("item name $3.45 +5 #wow")
-  #   assert_equal "item name",             parsed_item[:name]
-  #   assert_equal "3.45",                  parsed_item[:price]
-  #   assert_equal nil,                     parsed_item[:description_text]
-  #   assert_equal "5",                     parsed_item[:quantity]
-  #   assert_equal "wow",                   parsed_item[:cat1]
-  # end
-  # test "name price and category" do
-  #   parsed_item = ItemParser.parse("item name $3.45 #wow")
-  #   assert_equal "item name",             parsed_item[:name]
-  #   assert_equal "3.45",                  parsed_item[:price]
-  #   assert_equal nil,                     parsed_item[:description_text]
-  #   assert_equal nil,                     parsed_item[:quantity]
-  #   assert_equal "wow",                   parsed_item[:cat1]
-  # end
+  test "name price description and quantity" do
+    parsed_item = ItemParser.parse("item name $3.45 excellent condition +5")
+    assert_equal "item name",             parsed_item[:name]
+    assert_equal "3.45",                  parsed_item[:price]
+    assert_equal "excellent condition",  parsed_item[:description_text]
+    assert_equal "5",                     parsed_item[:quantity]
+    assert_equal nil,                     parsed_item[:cat1]
+  end
+  test "name price quantity and category" do
+    parsed_item = ItemParser.parse("item name $3.45 +5 #wow")
+    assert_equal "item name",             parsed_item[:name]
+    assert_equal "3.45",                  parsed_item[:price]
+    assert_equal nil,                     parsed_item[:description_text]
+    assert_equal "5",                     parsed_item[:quantity]
+    assert_equal "wow",                   parsed_item[:cat1]
+  end
+  test "name price and category" do
+    parsed_item = ItemParser.parse("item name $3.45 #wow")
+    assert_equal "item name",             parsed_item[:name]
+    assert_equal "3.45",                  parsed_item[:price]
+    assert_equal nil,                     parsed_item[:description_text]
+    assert_equal nil,                     parsed_item[:quantity]
+    assert_equal "wow",                   parsed_item[:cat1]
+  end
   # test "name price description and category" do
   #   parsed_item = ItemParser.parse("item name $3.45 excellent condition #wow")
   #   assert_equal "item name",             parsed_item[:name]
