a7e87bf5f47eb87855bbfe052ad17b71ca3c66ed
John Gallagher <john@jgediting.co.uk>
2011-11-01 18:20:29 +0000

    Added failing test.

--- test/unit/item_matcher_test.rb
+++ test/unit/item_matcher_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 class ItemMatcherTest < ActiveSupport::TestCase
-  test "one item with same name, price and description should match" do
+  # with one item
+  test "item with same name, price and description should match" do
     wallet = Factory.build(:wallet_item)
     found_item = ItemMatcher.new(:items => [wallet], :criteria => {:name => "wallet", :description_text => "this is a gorgeous wallet", :price => 22.22}).match
     assert_equal(wallet, found_item)
+  end
+  test "item with same name, different price and description should not match" do
+    wallet = Factory.build(:wallet_item)
+    found_item = ItemMatcher.new(:items => [wallet], :criteria => {:name => "wallet", :description_text => "this is a wallet", :price => 12.22}).match
+    assert_nil(found_item)
   end
 end
