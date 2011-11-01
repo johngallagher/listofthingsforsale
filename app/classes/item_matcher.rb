065b541fab6297c4f4b49bbe8fab4be03a0f3d06
John Gallagher <john@jgediting.co.uk>
2011-11-01 18:40:03 +0000

    Now we can find an item with only two factors specified.

--- app/classes/item_matcher.rb
+++ app/classes/item_matcher.rb
@@ -5,5 +5,8 @@ class ItemMatcher
   end
   def match
     perfect_item_match = @items.select{ |i| i.name == @criteria[:name] and i.price == @criteria[:price] and i.description_text == @criteria[:description_text] }.first
+    return perfect_item_match unless perfect_item_match.nil?
+    
+    name_and_price_match = @items.select{ |i| i.name == @criteria[:name] and i.price == @criteria[:price]}.first
   end
 end
--- test/unit/item_matcher_test.rb
+++ test/unit/item_matcher_test.rb
@@ -11,4 +11,9 @@ class ItemMatcherTest < ActiveSupport::TestCase
     found_item = ItemMatcher.new(:items => [wallet], :criteria => {:name => "wallet", :description_text => "this is a wallet", :price => 12.22}).match
     assert_nil(found_item)
   end
+  test "item with same name and price, different description should match" do
+    wallet = Factory.build(:wallet_item)
+    found_item = ItemMatcher.new(:items => [wallet], :criteria => {:name => "wallet", :description_text => "this is a wallet", :price => 22.22}).match
+    assert_equal(wallet, found_item)
+  end
 end
