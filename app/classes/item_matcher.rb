fc19a45cb7bc150a3e4d478a1c24b9e9ee6859d6
John Gallagher <john@jgediting.co.uk>
2011-11-01 18:34:47 +0000

    Neatened up passing test.

--- app/classes/item_matcher.rb
+++ app/classes/item_matcher.rb
@@ -5,10 +5,5 @@ class ItemMatcher
   end
   def match
     perfect_item_match = @items.select{ |i| i.name == @criteria[:name] and i.price == @criteria[:price] and i.description_text == @criteria[:description_text] }.first
-    if perfect_item_match
-      perfect_item_match
-    else
-      nil
-    end
   end
 end
