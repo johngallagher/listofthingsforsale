58d5e87d9f50895a95c907bcf7646e175c06d011
John Gallagher <john@jgediting.co.uk>
2011-11-05 17:04:17 +0000

    Basic test passed - spaces are now changed for hyphens

--- app/classes/category_name_sanitizer.rb
+++ app/classes/category_name_sanitizer.rb
@@ -16,13 +16,18 @@ class CategoryNameSanitizer
     @name.each_byte do |char_code|
       cleaned_char = cleaned_char(char_code.chr, char_index)
       cleaned_name << cleaned_char
-      # char_index++
+      char_index++
     end
     cleaned_name
   end
   
   def cleaned_char(this_char, char_index)
-    this_char = "-" if this_char == " "
+    if char_index == 1
+      this_char = "" if this_char.match(/^[a-zA-Z]$/).nil?
+    else
+      this_char = "-" if this_char == " "
+      this_char = "" if this_char.match(/^[_a-zA-Z0-9-]$/).nil?
+    end
     this_char
   end
 end
