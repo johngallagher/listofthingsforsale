876de37ff1f4cc43f968560ea79dce1126bb5f84
John Gallagher <john@jgediting.co.uk>
2011-11-10 13:02:58 +0000

    Changed how the method works by having supported currencies. Messy.

--- app/classes/currency_switcher.rb
+++ app/classes/currency_switcher.rb
@@ -2,17 +2,19 @@
 class CurrencySwitcher
   def initialize(args)
     @description = args
-    @supported_currencies = [Currency::USD, Currency::GBP]
   end
   def get_currency
-    pound_matches = @description.scan(/(?<name>[[:print:]]+) £(?<price>\d+\.*\d*)( +?(?<description_text>[^#\+][[:print:]]*?))?( +?(\+(?<quantity>\d+)))?( +?#(?<category_1>[[:print:]][^#]*))?( +?#(?<category_2>[[:print:]][^#]*))?( +?#(?<category_3>[[:print:]][^#]*))?( +?#(?<category_4>[[:print:]][^#]*))?( +?#(?<category_5>[[:print:]][^#]*))?( +?#(?<category_6>[[:print:]][^#]*))?$/m)
-    dollar_matches = @description.scan(/(?<name>[[:print:]]+) \$(?<price>\d+\.*\d*)( +?(?<description_text>[^#\+][[:print:]]*?))?( +?(\+(?<quantity>\d+)))?( +?#(?<category_1>[[:print:]][^#]*))?( +?#(?<category_2>[[:print:]][^#]*))?( +?#(?<category_3>[[:print:]][^#]*))?( +?#(?<category_4>[[:print:]][^#]*))?( +?#(?<category_5>[[:print:]][^#]*))?( +?#(?<category_6>[[:print:]][^#]*))?$/m)
-    pound_matches ||= ""
-    dollar_matches ||= ""
-    if pound_matches.count > dollar_matches.count
-      Currency::GBP
-    else
-      Currency::USD
+    winning_currency = Currency::Supported.first
+    highest_matches = 0
+    Currency::Supported.each do |this_currency|
+      escaped_currency_symbol = Currency::EscapedSymbol[this_currency]
+      this_currency_matches = @description.scan(/(?<name>[[:print:]]+) #{escaped_currency_symbol}(?<price>\d+\.*\d*)( +?(?<description_text>[^#\+][[:print:]]*?))?( +?(\+(?<quantity>\d+)))?( +?#(?<category_1>[[:print:]][^#]*))?( +?#(?<category_2>[[:print:]][^#]*))?( +?#(?<category_3>[[:print:]][^#]*))?( +?#(?<category_4>[[:print:]][^#]*))?( +?#(?<category_5>[[:print:]][^#]*))?( +?#(?<category_6>[[:print:]][^#]*))?$/m)
+      this_currency_matches ||= []
+      if this_currency_matches.count > highest_matches
+        highest_matches = this_currency_matches.count 
+        winning_currency = this_currency
+      end
     end
+    winning_currency
   end
 end
--- app/modules/currency.rb
+++ app/modules/currency.rb
@@ -1,6 +1,15 @@
+# encoding: UTF-8
 module Currency
   GBP = "GBP"
   USD = "USD"
   
-  Supported = [Currency::GBP, Currency::USD]
+  Supported = [Currency::USD, Currency::GBP] # in order of preference - default one first
+  Symbol = {
+    Currency::GBP => '£',
+    Currency::USD => '$'
+  }
+  EscapedSymbol = {
+    Currency::GBP => '£',
+    Currency::USD => '\$'
+  }
 end
