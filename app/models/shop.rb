90ae06d006d9fc32be257b7ae71de578fedd15ba
John Gallagher <john@jgediting.co.uk>
2011-11-11 21:04:22 +0000

    Before save we set the currency on the shop automatically.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ActiveRecord::Base
+  before_save :update_currency
+  
   has_attached_file :profile_photo, :styles => { :profile => "270x220>" }, :storage => :s3, :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml", :path => "/:style/:id/:filename"
   has_friendly_id :url, :use_slug => true
 
@@ -15,9 +17,14 @@ class Shop < ActiveRecord::Base
     self.payment_type ||= Payments::Paypal
     self.publishing_errors ||= []
     self.about_me ||= "Write whatever you want here. You could use it to tell people who you are and what you are selling. If you have a retail presence or a market stall, you could put up the details up here."
+    self.currency ||= Currency::USD
   end
   
   def profile_photo_height
     Paperclip::Geometry.from_file(profile_photo.to_file(:profile)).height
+  end
+  
+  def update_currency
+    self.currency = CurrencySwitcher.new(self.description).get_currency
   end
 end
--- spec/unit/currency_switcher_spec.rb
+++ spec/unit/currency_switcher_spec.rb
@@ -1,31 +1,31 @@
 # encoding: UTF-8
-require 'test_helper'
+require 'spec_helper'
 
-class CurrencySwitcherTest < ActiveSupport::TestCase
-  test "should default to dollars for empty string" do
+describe "CurrencySwitcher" do
+  it "should default to dollars for empty string" do
     assert_equal(Currency::USD, CurrencySwitcher.new('').get_currency)
   end
-  test "should be pounds for single price" do
+  it "should be pounds for single price" do
     currency = CurrencySwitcher.new('name £3').get_currency
     assert_equal(Currency::GBP, currency)
   end
-  test "should be pounds for single price with pence" do
+  it "should be pounds for single price with pence" do
     currency = CurrencySwitcher.new('name £3.00').get_currency
     assert_equal(Currency::GBP, currency)
   end
-  test "should be dollars for single price" do
+  it "should be dollars for single price" do
     currency = CurrencySwitcher.new('name $3').get_currency
     assert_equal(Currency::USD, currency)
   end
-  test "should be dollars for single price with pence" do
+  it "should be dollars for single price with pence" do
     currency = CurrencySwitcher.new('name $3.00').get_currency
     assert_equal(Currency::USD, currency)
   end
-  test "should be dollars for equal number of prices" do
+  it "should be dollars for equal number of prices" do
     currency = CurrencySwitcher.new("name $3 \nname £3").get_currency
     assert_equal(Currency::USD, currency)
   end
-  test "should handle multiple lines" do
+  it "should handle multiple lines" do
     currency = CurrencySwitcher.new("name $3\nname  £3\nname £3").get_currency
     assert_equal(Currency::GBP, currency)
   end
